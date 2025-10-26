7c8d741eaba02e40d8f79f42980470af0d0e4214
ytproze-ctrl <ytproze@gmail.com>
2025-10-26 10:52:52 +0100

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+// Remplacez ces valeurs par celles de votre projet Firebase
+const FIREBASE_CONFIG = {
+  apiKey: "VOTRE_APIKEY",
+  authDomain: "VOTRE_AUTHDOMAIN",
+  databaseURL: "VOTRE_DATABASE_URL",
+  projectId: "VOTRE_PROJECTID",
+  storageBucket: "VOTRE_STORAGEBUCKET",
+  messagingSenderId: "VOTRE_MESSAGING_SENDER_ID",
+  appId: "VOTRE_APPID",
+};
+
+export default FIREBASE_CONFIG;
